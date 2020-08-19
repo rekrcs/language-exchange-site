02d7d23796401631b43e3e1d0f242a8d95f78f28
rekrcs <rekrcs@gmail.com>
2020-08-19 09:37:14 +0900

    jsp연결

--- src/main/java/com/project/byk/le/controller/HomeController.java
+++ src/main/java/com/project/byk/le/controller/HomeController.java
@@ -7,9 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home/main")
-	@ResponseBody
+	@RequestMapping("usr/home/main")
 	public String showMain() {
-		return "안녕";
+		return "home/main";
 	}
 }
