4a79596b90b9db65b0bb039d94f74b3ab637e767
rekrcs <rekrcs@gmail.com>
2020-08-20 10:29:10 +0900

    회원 가입인사메일

--- src/main/java/com/project/byk/le/service/MemberService.java
+++ src/main/java/com/project/byk/le/service/MemberService.java
@@ -2,9 +2,8 @@ package com.project.byk.le.service;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.project.byk.le.dao.MemberDao;
@@ -14,10 +13,26 @@ import com.project.byk.le.dto.Member;
 public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
+	@Autowired
+	private MailService mailService;
+	@Value("${custom.siteMainUri}")
+	private String siteMainUri;
+	@Value("${custom.siteName}")
+	private String siteName;
 
 	public int join(Map<String, Object> param) {
-		
+		sendJoinCompleteMail((String) param.get("email"));
 		return memberDao.join(param);
+	}
+
+	private void sendJoinCompleteMail(String email) {
+		String mailTitle = String.format("[%s] 가입이 완료되었습니다.", siteName);
+
+		StringBuilder mailBodySb = new StringBuilder();
+		mailBodySb.append("<h1>가입이 완료되었습니다.</h1>");
+		mailBodySb.append(String.format("<p><a href=\"%s\" target=\"_blank\">%s</a>로 이동</p>", siteMainUri, siteName));
+
+		mailService.send(email, mailTitle, mailBodySb.toString());
 	}
 
 	public Member login(Map<String, Object> param) {
